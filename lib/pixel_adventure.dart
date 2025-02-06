811954bb63921a56d0b40677c1f8e1bf2eadd73f
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-02-05 22:50:44 -0300

    :sparkles: feat(public): adiciona movimentação básica do player

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -2,10 +2,13 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame
+  with HasKeyboardHandlerComponents {
+
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
